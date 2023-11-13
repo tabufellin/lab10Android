9105883566ea64af83ba4fbbcfcd986df9c1c406
sebastiangonzalez <dsgspersonal@gmail.com>
2023-11-13 09:40:41 +0100

    a

--- app/src/main/java/com/example/lab10/AuthActivity.kt
+++ app/src/main/java/com/example/lab10/AuthActivity.kt
@@ -1,27 +1,30 @@
 package com.example.lab10
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.FilterQueryProvider
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lab10.ui.theme.Lab10Theme
+import androidx.appcompat.resources.Compatibility.Api15Impl
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
+
+    private val GOOGLE_SIGN_IN = 100
 
     private lateinit var firebaseAnalytics: FirebaseAnalytics
 
@@ -42,6 +45,34 @@ class MainActivity : ComponentActivity() {
 
         setup()
 
+        //session
+        session()
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val layoutAuth : LinearLayout  = findViewById(R.id.authLayout) as LinearLayout
+        layoutAuth.visibility = View.VISIBLE
+    }
+
+    private fun session () {
+
+        val prefs : SharedPreferences = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email: String? = prefs.getString("email", null)
+        val provider: String? = prefs.getString("provider", null)
+
+        if (email != null && provider != null) {
+
+            //
+            val layoutAuth : LinearLayout  = findViewById(R.id.authLayout) as LinearLayout
+            layoutAuth.visibility = View.INVISIBLE
+            //
+
+            showHome(email, ProviderType.valueOf(provider))
+        }
 
     }
 
@@ -51,6 +82,8 @@ class MainActivity : ComponentActivity() {
 
         val signUpButton = findViewById(R.id.signUpButton) as Button
         val logInButton = findViewById(R.id.logInButton) as Button
+        val googleButton = findViewById(R.id.googleButton) as Button
+
 
         val emailEditText = findViewById(R.id.emailEditText) as EditText
         val passwordEditText = findViewById(R.id.passwordEditText) as EditText
@@ -94,6 +127,22 @@ class MainActivity : ComponentActivity() {
             //Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
         }
 
+        googleButton.setOnClickListener {
+
+            //config
+
+            val googleConf  = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("526193774636-dq9u0u99d2b5tmr25bo3lqc37k8k4m4o.apps.googleusercontent.com")
+                .requestEmail()
+                .build()
+
+            val googleClient : GoogleSignInClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN )
+
+        }
+
     }
 
     private fun showAlert (error: String) {
@@ -113,5 +162,48 @@ class MainActivity : ComponentActivity() {
         }
         startActivity(homeIntent)
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account : GoogleSignInAccount? = task.getResult(ApiException::class.java)
+
+
+                if (account != null && account.idToken != null) {
+
+                    val credential:AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(account.email.toString() ?: "", ProviderType.GOOGLE )
+
+                        } else {
+                            showAlert(it.exception?.toString() ?: "")
+                        }
+                    }
+
+
+
+                } else {
+                    if (account != null) {
+                        showAlert("${account.id} ${account.idToken} ERROR ID TOKEN")
+                    } else {
+                        showAlert(" ERROR ID TOKEN NULL ESTAMOS FRITOS")
+
+                    }
+                }
+            } catch ( e: ApiException) {
+                showAlert("CAI EN EL PRIMER CATCHH EL DE HASTA ARRIBA ${e.toString()}")
+            }
+
+
+
+        }
     }
 }
--- app/src/main/java/com/example/lab10/HomeActivity.kt
+++ app/src/main/java/com/example/lab10/HomeActivity.kt
@@ -1,13 +1,69 @@
 package com.example.lab10
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import com.google.firebase.auth.FirebaseAuth
 
 enum class ProviderType {
-    BASIC
+    BASIC,
+    GOOGLE
 }
 class HomeActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Setup
+
+        val bundle : Bundle? = intent.extras
+        val email = bundle?.getString("email") ?: ""
+        val provider = bundle?.getString("provider") ?: ""
+
+        setup(email, provider)
+
+        //SAVE VALUES
+
+        val prefs : SharedPreferences.Editor = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
+
+    }
+
+    private fun setup (email :String, provider: String) {
+        title = "Inicio"
+
+        val emailTextView = findViewById(R.id.emailTextView) as TextView
+        val providerTextView = findViewById(R.id.providerTextView) as TextView
+        val logOutButton = findViewById(R.id.logOutButton) as Button
+
+
+        emailTextView.text = email
+        providerTextView.text = provider
+
+
+        //button
+
+        logOutButton.setOnClickListener {
+
+            //clear data of current user
+            val prefs : SharedPreferences.Editor = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+
+
+
+
+
+
     }
 }
